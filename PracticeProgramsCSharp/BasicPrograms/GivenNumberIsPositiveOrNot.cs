825695cb428879161ec0b8817828efb0c780a98b
hxvrdn <122196725+hxvrdn@users.noreply.github.com>
2023-08-11 16:51:17 +0530

    Program for Even Odd Checking, added.

---
+++ PracticeProgramsCSharp/BasicPrograms/GivenNumberIsPositiveOrNot.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProgramsCSharp.BasicPrograms
+{
+    class GivenNumberIsPositiveOrNot
+    {
+
+    public static void logic()
+        {
+        start:
+            Console.Clear();
+            Console.WriteLine("Enter a Number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if(num ==0)
+            {
+                Console.WriteLine("Number is 0");
+                Console.WriteLine("Press 1 to ReRun");
+
+            }
+            else if( num < 0)
+            {
+                Console.WriteLine("Number Is NEGATIVE");
+                Console.WriteLine("Press 1 to ReRun");
+
+            }
+
+            else
+            {
+                Console.WriteLine("Number Is POSITIVE");
+                Console.WriteLine("Press 1 to ReRun");
+
+            }
+            int rerun = Convert.ToInt32(Console.ReadLine());
+            if (rerun == 1)
+                goto start;
+        }
+    }
+}
