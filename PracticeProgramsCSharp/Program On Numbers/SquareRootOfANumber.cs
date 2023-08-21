a76781afd35996f81c4d8d6ab2f50a63e25be95a
hxvrdn <122196725+hxvrdn@users.noreply.github.com>
2023-08-21 17:35:26 +0530

    Calculate Squareroot of a number.

---
+++ PracticeProgramsCSharp/Program On Numbers/SquareRootOfANumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProgramsCSharp.Program_On_Numbers
+{
+    class SquareRootOfANumber
+    {
+        public static void logic()
+        {
+        start:
+            Console.Clear();
+            Console.WriteLine("Enter the number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            double sqnum = Math.Sqrt(num);
+
+            Console.WriteLine("Square Root Of {0} is {1}", num, sqnum);
+
+            Console.WriteLine("\n Press 1 To rerun the program \n Press 0 to Main Menu");
+            int rerun = 0;
+            rerun = Convert.ToInt32(Console.ReadLine());
+
+            if (rerun == 1)
+                goto start;
+            else
+            {
+                Program.MainMenu();
+            }
+        }
+    }
+
+}
