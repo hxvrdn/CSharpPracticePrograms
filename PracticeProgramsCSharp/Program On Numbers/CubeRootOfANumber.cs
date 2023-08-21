be50997f4fa030a05e8e83041b9d0ca0f853ff73
hxvrdn <122196725+hxvrdn@users.noreply.github.com>
2023-08-21 17:35:42 +0530

    Cube Root Of A Number.

---
+++ PracticeProgramsCSharp/Program On Numbers/CubeRootOfANumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProgramsCSharp.Program_On_Numbers
+{
+    class CubeRootOfANumber
+    {
+        public static void logic()
+        {
+        start:
+            Console.Clear();
+            Console.WriteLine("Enter A Number");
+            double num = Convert.ToDouble(Console.ReadLine());
+
+           double res = Math.Ceiling(Math.Pow(num, (double)1 / 3));
+            Console.Write("Cube Root : " + res);
+
+            Console.WriteLine("\n Press 1 To rerun the program \n Press 0 to Main Menu");
+            int rerun = 0;
+            rerun = Convert.ToInt32(Console.ReadLine());
+
+            if (rerun == 1)
+                goto start;
+            else
+            {
+                Program.MainMenu();
+            }
+        }
+    }
+}
